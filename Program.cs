e84f418d6dfdfc7430a716c9538d932d1d4c1330
yuchiki <yuki08281993@gmail.com>
2021-09-02 20:23:23 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_nine_adt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
